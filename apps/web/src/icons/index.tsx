dd61d652a237d844622fd20fc064f1e85ed24a5f
abbesm0hamed <abbesmohamed717@gmail.com>
2026-07-17 18:46:43 +0100

    refactor: more stable layout

---
+++ apps/web/src/icons/index.tsx
@@ -0,0 +1,13 @@
+import {
+  RiArrowLeftSLine,
+  RiArrowRightSLine,
+  RiArrowUpSLine,
+  RiArrowDownSLine,
+} from "@remixicon/react";
+
+export const Icons = {
+  ArrowDown: RiArrowDownSLine,
+  ArrowLeft: RiArrowLeftSLine,
+  ArrowRight: RiArrowRightSLine,
+  ArrowUp: RiArrowUpSLine,
+};
